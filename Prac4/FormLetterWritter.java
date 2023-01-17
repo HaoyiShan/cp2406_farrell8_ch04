8c08d09c7d7649f18bc5b8c0b5c1690e5fb0b5ea
HaoyiShan <110078010+HaoyiShan@users.noreply.github.com>
2023-01-17 11:28:16 +0800

    debug

---
+++ Prac4/FormLetterWritter.java
@@ -0,0 +1,43 @@
+public class FormLetterWritter {
+    public static void main(String[] args)
+
+    {
+
+        displaySalutation("DK");
+
+        displayLetter();
+
+        displaySalutation("Nian", "Qian");
+
+        displayLetter();
+
+    }
+
+
+
+    public static void displaySalutation(String lastName)
+
+    {
+
+        System.out.println("Dear Mr. or Ms. " + lastName + ",");
+
+    }
+
+
+
+    public static void displaySalutation(String firstName, String lastName)
+
+    {
+
+        System.out.println("Dear " + firstName + " " + lastName + ",");
+
+    }
+
+    public static void displayLetter()
+
+    {
+
+        System.out.println("   Thank you for your recent order.\n");
+
+    }
+}
